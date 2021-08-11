081f1055f22ac9001931e86076fc6a04e8fb8d9e
Jacob Gilbert <jacob.gilbert@protonmail.com>
2021-08-10 20:47:35 -0600

    adding GR-3.9 branch
    Signed-off-by: Jacob Gilbert <jacob.gilbert@protonmail.com>

--- lib/GridStream_impl.h
+++ lib/GridStream_impl.h
@@ -2,20 +2,7 @@
 /*
  * Copyright 2021 Hash.
  *
- * This is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3, or (at your option)
- * any later version.
- *
- * This software is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this software; see the file COPYING.  If not, write to
- * the Free Software Foundation, Inc., 51 Franklin Street,
- * Boston, MA 02110-1301, USA.
+ * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
 #ifndef INCLUDED_SMART_METERS_GRIDSTREAM_IMPL_H
@@ -25,31 +12,35 @@
 #include <smart_meters/constants.h>
 
 namespace gr {
-  namespace smart_meters {
+namespace smart_meters {
 
-    class GridStream_impl : public GridStream
-    {
-     private:
-		bool d_crcEnable;
-		uint16_t d_crcInitialValue;
-		uint32_t d_meterMonitorID;
-		uint8_t d_packetTypeFilter;
-		uint16_t d_packetLengthFilter;
-        uint16_t crc16(uint16_t crc, const std::vector<uint8_t> &data, size_t size);
+class GridStream_impl : public GridStream
+{
+private:
+    bool d_crcEnable;
+    uint16_t d_crcInitialValue;
+    uint32_t d_meterMonitorID;
+    uint8_t d_packetTypeFilter;
+    uint16_t d_packetLengthFilter;
+    uint16_t crc16(uint16_t crc, const std::vector<uint8_t>& data, size_t size);
 
-     //protected:
-     // int calculate_output_stream_length(const gr_vector_int &ninput_items);
+    /*!
+     * \brief Message handler for input messages
+     *
+     * \param msg Dict PMT or PDU message passed from the scheduler's message handling.
+     */
+    void pdu_handler(pmt::pmt_t pdu);
 
-     public:
-      GridStream_impl(bool crcEnable, uint16_t crcInitialValue, uint32_t meterMonitorID, uint8_t packetTypeFilter, uint16_t packetLengthFilter);
-      ~GridStream_impl();
+public:
+    GridStream_impl(bool crcEnable,
+                    uint16_t crcInitialValue,
+                    uint32_t meterMonitorID,
+                    uint8_t packetTypeFilter,
+                    uint16_t packetLengthFilter);
+    ~GridStream_impl() override;
+};
 
-      // Where all the action really happens
-      void pdu_handler(pmt::pmt_t pdu);
-    };
-
-  } // namespace smart_meters
+} // namespace smart_meters
 } // namespace gr
 
 #endif /* INCLUDED_SMART_METERS_GRIDSTREAM_IMPL_H */
-
