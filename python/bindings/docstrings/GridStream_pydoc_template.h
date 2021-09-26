5e47b5d5211312ddf83b2615b2cd059dfd5758aa
BitBangingBytes <38797169+BitBangingBytes@users.noreply.github.com>
2021-09-26 15:54:26 -0500

    Added Documentation and new PMT Dict's

--- python/bindings/docstrings/GridStream_pydoc_template.h
+++ python/bindings/docstrings/GridStream_pydoc_template.h
@@ -7,7 +7,7 @@
  *
  */
 #include "pydoc_macros.h"
-#define D(...) DOC(gr, smart_meters, __VA_ARGS__)
+#define D(...) DOC(gr,smart_meters, __VA_ARGS__ )
 /*
   This file contains placeholders for docstrings for the Python bindings.
   Do not edit! These were automatically extracted during the binding process
@@ -15,7 +15,13 @@
  */
 
 
-static const char* __doc_gr_smart_meters_GridStream = R"doc()doc";
+ 
+ static const char *__doc_gr_smart_meters_GridStream = R"doc()doc";
 
 
-static const char* __doc_gr_smart_meters_GridStream_make = R"doc()doc";
+ static const char *__doc_gr_smart_meters_GridStream_GridStream = R"doc()doc";
+
+
+ static const char *__doc_gr_smart_meters_GridStream_make = R"doc()doc";
+
+  
