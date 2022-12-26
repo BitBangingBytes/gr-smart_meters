b9a9da386654c58a3310cafee4e80e878548b153
BitBangingBytes <hash@bitbangingbytes.com>
2022-12-26 11:31:42 -0600

    Updated test to compare PDU's and fixed data types

--- python/qa_GridStream.py
+++ python/qa_GridStream.py
@@ -29,10 +29,7 @@ class qa_GridStream(gr_unittest.TestCase):
         self.debug = blocks.message_debug()
         self.in_meta = pmt.make_dict()
         self.expected_meta = pmt.make_dict()
-        # self.base_dict = pmt.dict_add(self.base_dict, pmt.intern("key1"), pmt.intern("value1"))
-        # self.base_dict = pmt.dict_add(self.base_dict, pmt.intern("key2"), pmt.intern("value2"))
-        # self.base_dict = pmt.dict_add(self.base_dict, pmt.intern("uint64_key"), pmt.from_uint64(1234567))
-        # self.base_dict = pmt.dict_add(self.base_dict, pmt.intern("double_key"), pmt.from_double(1.234567))
+        self.expected_pdu = 0
 
     def connectUp(self):
         self.tb.msg_connect((self.emitter, 'msg'), (self.block_under_test, 'pdu_in'))
@@ -48,12 +45,9 @@ class qa_GridStream(gr_unittest.TestCase):
                                            0x5FD6, 0, 0, 0x00, 0x00)
         self.connectUp()
 
-        in_data = [0x00, 0xff, 0x2a, 0x55, 0x00, 0x23, 0x30, 0xff, 
-                   0xff, 0xff, 0xff, 0xff, 0xff, 0x50, 0xcf, 0xfd, 
-                   0xd9, 0xe4, 0xe0, 0xb2, 0x01, 0x0f, 0x27, 0x48, 
-                   0xa4, 0x83, 0xf0, 0x6e, 0xbc, 0x01, 0x01, 0x00, 
-                   0x18, 0x66, 0x03, 0x39, 0xf3, 0x7e, 0x90, 0xdc, 
-                   0xdb, 0x04]
+        in_data = [0x00, 0xff, 0x2a, 0x55, 0x00, 0x23, 0x30, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x50, 0xcf, 0xfd, 
+                   0xd9, 0xe4, 0xe0, 0xb2, 0x01, 0x0f, 0x27, 0x48, 0xa4, 0x83, 0xf0, 0x6e, 0xbc, 0x01, 0x01, 0x00, 
+                   0x18, 0x66, 0x03, 0x39, 0xf3, 0x7e, 0x90, 0xdc, 0xdb, 0x04]
         expected_data = in_data
 
         self.in_meta = pmt.dict_add(self.in_meta, pmt.intern("center_frequency"), pmt.from_double(915300000))
@@ -63,8 +57,10 @@ class qa_GridStream(gr_unittest.TestCase):
         self.expected_meta = pmt.dict_add(self.expected_meta, pmt.intern("Gridstream_LanSrcID"), pmt.intern("F06EBC01"))
         self.expected_meta = pmt.dict_add(self.expected_meta, pmt.intern("Gridstream_WanSrcID"), pmt.intern("50CFFDD9E4E0"))
         self.expected_meta = pmt.dict_add(self.expected_meta, pmt.intern("Gridstream_LanDstID"), pmt.intern(""))
-        self.expected_meta = pmt.dict_add(self.expected_meta, pmt.intern("Gridstream_Uptime"), pmt.from_uint64(17770312))
+        self.expected_meta = pmt.dict_add(self.expected_meta, pmt.intern("Gridstream_Uptime"), pmt.from_long(17770312))
         self.expected_meta = pmt.dict_add(self.expected_meta, pmt.intern("Gridstream_Freq"), pmt.from_double(915.3))
+
+        self.expected_pdu = pmt.cons(self.expected_meta, pmt.init_u8vector(len(expected_data), expected_data))
 
         self.tb.start()
         time.sleep(.001)
@@ -73,12 +69,8 @@ class qa_GridStream(gr_unittest.TestCase):
         self.tb.stop()
         self.tb.wait()
 
-        data = pmt.cdr(self.debug.get_message(0))
-        meta = pmt.car(self.debug.get_message(0))
-        
         self.assertEqual(1, self.debug.num_messages())
-        self.assertListEqual(list(pmt.to_python(data)), expected_data)
-        self.assertDictEqual(pmt.to_python(meta), pmt.to_python(self.expected_meta))
+        self.assertTrue(pmt.equal(self.debug.get_message(0), self.expected_pdu))
 
     def test_002_partial_packet_pass_through(self):
         # GridStream(eCRC, eDEBUG, eTimeStamp, eEpoch, eFrequency, eBaudRate, 
@@ -90,7 +82,10 @@ class qa_GridStream(gr_unittest.TestCase):
         in_data = [0x00, 0xff, 0x2a, 0x55, 0x00, 0x23, 0x30, 0xff]
         expected_data = in_data
 
-        in_pdu = pmt.cons(pmt.make_dict(), pmt.init_u8vector(len(in_data), in_data))
+        self.in_meta = pmt.dict_add(self.in_meta, pmt.intern("center_frequency"), pmt.from_double(915300000))
+        in_pdu = pmt.cons(self.in_meta, pmt.init_u8vector(len(in_data), in_data))
+        
+        expected_pdu = pmt.cons(self.in_meta, pmt.init_u8vector(len(expected_data), expected_data))
 
         self.tb.start()
         time.sleep(.001)
@@ -99,10 +94,8 @@ class qa_GridStream(gr_unittest.TestCase):
         self.tb.stop()
         self.tb.wait()
 
-        data = pmt.cdr(self.debug.get_message(0))
-
         self.assertEqual(1, self.debug.num_messages())
-        self.assertTrue(data, expected_data)
+        self.assertTrue(pmt.equal(in_pdu, expected_pdu))
 
     def test_003_passing_crc_pass_through(self):
         # GridStream(eCRC, eDEBUG, eTimeStamp, eEpoch, eFrequency, eBaudRate, 
