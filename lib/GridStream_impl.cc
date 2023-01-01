84f6dcc8d27d9722e3a5566e2af650d8af044354
Hash <38797169+BitBangingBytes@users.noreply.github.com>
2022-12-31 18:54:19 -0600

    Removed comments/extra space from time function

--- lib/GridStream_impl.cc
+++ lib/GridStream_impl.cc
@@ -100,24 +100,13 @@ std::string time_in_HH_MM_SS_MMM()
 {
     using namespace std::chrono;
 
-    // get current time
     auto now = system_clock::now();
-
-    // get number of milliseconds for the current second
-    // (remainder after division into seconds)
     auto ms = duration_cast<milliseconds>(now.time_since_epoch()) % 1000;
-
-    // convert to std::time_t in order to convert to std::tm (broken time)
     auto timer = system_clock::to_time_t(now);
-
-    // convert to broken time
     std::tm bt = *std::localtime(&timer);
-
     std::ostringstream oss;
-
     oss << std::put_time(&bt, "%H:%M:%S"); // HH:MM:SS
     oss << '.' << std::setfill('0') << std::setw(3) << ms.count();
-
     return oss.str();
 }
 
